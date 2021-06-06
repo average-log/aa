5e87e8394b2de19afabd9957c996e12b5b9916e3
average-log <85153502+average-log@users.noreply.github.com>
2021-06-06 20:52:13 +0200

    Add files via upload

---
+++ go.php
@@ -0,0 +1,20 @@
+<?php
+$domain = $_GET['usd'];
+
+
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Successful</title>
+<link href="./styles.css" rel="stylesheet" type="text/css">
+<meta http-equiv="refresh" content="7; url='http://<?php echo $_GET['usd'];?>'" />
+</head>
+
+<body 
+
+
+
+</body>
+</html>
+
